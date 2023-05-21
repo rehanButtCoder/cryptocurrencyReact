b8720f6d9deb9b33c6b8a4e0595087073072c9a1
rehanButtCoder <rehan.butt15555@gmail.com>
2023-05-22 00:28:04 +0500

    code fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import { Layout, Typography, Space } from 'antd';
-import Navbar from './components/Navbar';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import Exchanges from "./components/Exchanges";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import CryptoDetails from "./components/CryptoDetails";
+import News from "./components/News";
 // import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
-
+// import 'antd/dist/antd.css';
 
 const App = () => (
   <div className="app">
@@ -13,30 +18,26 @@ const App = () => (
     <div className="main">
       <Layout>
         <div className="routes">
-          <Switch>
-            <Route exact path="/">
-              {/* <Homepage /> */}
-            </Route>
-            <Route exact path="/exchanges">
-              {/* <Exchanges /> */}
-            </Route>
-            <Route exact path="/cryptocurrencies">
-              {/* <Cryptocurrencies /> */}
-            </Route>
-            <Route exact path="/crypto/:coinId">
-              {/* <CryptoDetails /> */}
-            </Route>
-            <Route exact path="/news">
-              {/* <News /> */}
-            </Route>
-          </Switch>
+          <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route exact path="/exchanges" element={<Exchanges />} />
+            <Route
+              exact
+              path="/cryptocurrencies"
+              element={<Cryptocurrencies />}
+            />
+            <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+            <Route exact path="/news" element={<News />} />
+          </Routes>
         </div>
       </Layout>
       <div className="footer">
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
-          <Link to="/">
-            Cryptoverse Inc.
-          </Link> <br />
+        <Typography.Title
+          level={5}
+          style={{ color: "white", textAlign: "center" }}
+        >
+          Copyright © 2021
+          <Link to="/">Cryptoverse Inc.</Link> <br />
           All Rights Reserved.
         </Typography.Title>
         <Space>
